b261b8f2145bb8a42e34656f4b85e42edbdaf3d7
Martin Krupicka <martin.x.krupicka@seznam.cz>
2025-05-10 18:07:42 +0200

    partitioned hyprladn conf

--- home/dotfiles.nix
+++ home/dotfiles.nix
@@ -4,13 +4,16 @@
 	home.file.".tmux.conf".source         = ./dotfiles/.tmux.conf;
 	home.file.".ideavimrc".source         = ./dotfiles/.ideavimrc;
 	home.file.".config/kitty/kitty.conf".source = ./dotfiles/kitty.conf;
-	home.file.".config/hypr/hyprland.conf".source = ./dotfiles/hyprland.conf;
 	home.file.".config/waybar/config".source = ./dotfiles/waybar-config;
 	home.file.".gitconfig".source         = ./dotfiles/.gitconfig;
     home.file.".config/gtk-3.0/settings.ini".source = ./dotfiles/gtk-3.settings;
     home.file.".config/gtk-4.0/settings.ini".source = ./dotfiles/gtk-4.settings;
 	home.file.".config/nvim"= {
 		source = ./dotfiles/nvim;
+		recursive = true;
+	};
+	home.file.".config/hypr"= {
+		source = ./dotfiles/hypr;
 		recursive = true;
 	};
 }
