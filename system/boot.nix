04b1a97096a521f4bd85626b706ab17eb5ff5188
Martin Krupicka <martin.x.krupicka@seznam.cz>
2025-05-11 15:45:01 +0200

    added some stuff to make idle low consumption

--- system/boot.nix
+++ system/boot.nix
@@ -8,8 +8,20 @@
 
   boot.kernelParams = [
     "button.lid_init_state=open"
-  ];
 
+    "intel_idle.max_cstate=9"        # allow the deepest package C‐state
+    "processor.max_cstate=6"         # allow deep CPU C‐states
+
+    # Intel GPU runtime power‐management + panel self refresh
+    "i915.enable_rc6=1"              # enable RC6 sleep states
+    "i915.enable_psr=1"              # panel self‐refresh
+    "i915.enable_fbc=1"              # frame buffer compression (if supported)
+  ];
+  services.cpupower = {
+  enable   = true;
+  governor = "schedutil";   # or "ondemand"
+};
+services.power-profiles-daemon.enable = true;
   zramSwap = {
     enable        = true;    # turn on zram swap
     memoryPercent = 20;      # use 20% of RAM for compressed swap
