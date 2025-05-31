4f885ad94dd31d382268a2a773cc153711036969
Martin Krupicka <martin.x.krupicka@seznam.cz>
2025-05-31 18:46:24 +0200

    waybar jsonc

--- home/dotfiles.nix
+++ home/dotfiles.nix
@@ -4,7 +4,7 @@
 	home.file.".tmux.conf".source         = ./dotfiles/.tmux.conf;
 	home.file.".ideavimrc".source         = ./dotfiles/.ideavimrc;
 	home.file.".config/kitty/kitty.conf".source = ./dotfiles/kitty.conf;
-	home.file.".config/waybar/config".source = ./dotfiles/waybar-config;
+	home.file.".config/waybar/config".source = ./dotfiles/waybar-config.jsonc;
 	home.file.".gitconfig".source         = ./dotfiles/.gitconfig;
     home.file.".config/gtk-3.0/settings.ini".source = ./dotfiles/gtk-3.settings;
     home.file.".config/gtk-4.0/settings.ini".source = ./dotfiles/gtk-4.settings;
