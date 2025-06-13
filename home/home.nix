53fea43af2fa0784faa35c833a0e3cb06d707715
Martin Krupicka <martin.x.krupicka@seznam.cz>
2025-06-13 21:25:12 +0200

    aa

--- home/home.nix
+++ home/home.nix
@@ -5,9 +5,4 @@
   home.stateVersion  = "25.05";
 
   imports = [ ./programs.nix ./dotfiles.nix ./devStuff ./overlays.nix];
-
-    stylix = {
-    enable = true;                # turn on Stylix in HM
-    targets.kitty.enable = true;  # enable the Kitty theming target
-  };
 }
