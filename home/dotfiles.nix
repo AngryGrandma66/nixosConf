702d76313a837955a4ab204cb5b0e7b9fa988877
Martin Krupicka <martin.x.krupicka@seznam.cz>
2025-06-13 21:21:55 +0200

    a

--- home/dotfiles.nix
+++ home/dotfiles.nix
@@ -1,6 +1,5 @@
 { pkgs,config, ... }:
 {
-
     home.file.".zshrc".source             = ./dotfiles/.zshrc;
     home.file.".tmux.conf".source         = ./dotfiles/.tmux.conf;
     home.file.".ideavimrc".source         = ./dotfiles/.ideavimrc;
@@ -25,8 +24,25 @@
         source = ./dotfiles/waybar;
         recursive = true;
     };
+    home.file.".config/wallust"= {
+        source = ./dotfiles/wallust;
+        recursive = true;
+    };
+    home.file.".config/kitty"= {
+        source = ./dotfiles/kitty;
+        recursive = true;
+    };
+
+
+
+    home.file.".config/gtk-3.0/gtk.css".source         = ./dotfiles/gtk.css;
+    home.file.".config/gtk-4.0/gtk.css".source         = ./dotfiles/gtk.css;
     gtk = {
         iconTheme.name   = "BeautyLine";
         iconTheme.package = pkgs.beauty-line-icon-theme;
+        cursorTheme.name = "rose-pine-hyprcursor";
+        cursorTheme.package = pkgs.rose-pine-hyprcursor;
+        theme.name = "adw-gtk3";
+        theme.package= pkgs.adw-gtk3;
     };
 }
