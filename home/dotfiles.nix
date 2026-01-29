eaf2b984ace3b05b33bf5f8efc5e3532f58931d6
Martin Krupicka <martin.x.krupicka@seznam.cz>
2026-01-29 19:03:01 +0100

    Update flake before rebuild

--- home/dotfiles.nix
+++ home/dotfiles.nix
@@ -1,76 +1,78 @@
 { pkgs,config, ... }:
-let OUSL = config.lib.file.mkOutOfStoreSymlink;
- dotfilesPath = "/etc/nixos/home/dotfiles";
- dotfiles = subpath: OUSL "${dotfilesPath}/${subpath}";
-in {
- home.file.".zshrc".source = dotfiles ".zshrc";
- home.file.".tmux.conf".source = dotfiles ".tmux.conf";
- home.file.".ideavimrc".source = dotfiles ".ideavimrc";
- home.file.".gitconfig".source = dotfiles ".gitconfig";
+let 
+OUSL = config.lib.file.mkOutOfStoreSymlink;
 
- home.file.".config/nvim" = {
- source = dotfiles "nvim";
- recursive = true;
- };
+dotfilesPath= "/etc/nixos/home/dotfiles";
+dotfiles = subpath:
+OUSL "${dotfilesPath}/${subpath}";
+in
+{
+    home.file.".zshrc".source             =dotfiles ".zshrc";
+    home.file.".tmux.conf".source         =dotfiles ".tmux.conf";
+    home.file.".ideavimrc".source         =dotfiles ".ideavimrc";
+    home.file.".gitconfig".source         =dotfiles ".gitconfig";
+    home.file.".config/nvim"= {
+        source = dotfiles "nvim";
+        recursive = true;
+    };
+    home.file.".config/hypr"= {
+        source = dotfiles "hypr";
+    };
+# home.file.".config/swaync"= {
+#     source =dotfiles "swaync";
+# };
+    home.file.".config/waybar"= {
+        source =dotfiles "waybar";
+    };
+    home.file.".config/wallust"= {
+        source =dotfiles "wallust";
+    };
+    home.file.".config/kitty"= {
+        source =dotfiles "kitty";
+    };
+    home.file.".config/rofi/config.rasi".source         =dotfiles "rofi-config.rasi";
 
- home.file.".config/hypr" = {
- source = dotfiles "hypr";
- recursive = true;
- };
+    home.file.".config/quickshell"= {
+        source =dotfiles "quickshell";
+    };
 
- home.file.".config/waybar" = {
- source = dotfiles "waybar";
- recursive = true;
- };
 
- home.file.".config/wallust" = {
- source = dotfiles "wallust";
- recursive = true;
- };
+    home.file.".config/gtk-3.0/gtk.css".source         =dotfiles "gtk.css";
+    home.file.".config/gtk-4.0/custom.css".source         = dotfiles "gtk.css";
 
- home.file.".config/kitty" = {
- source = dotfiles "kitty";
- recursive = true;
- };
+    gtk = {
+        enable = true;
+        iconTheme.name   = "BeautyLine";
+        iconTheme.package = pkgs.beauty-line-icon-theme;
+        cursorTheme.name = "rose-pine-hyprcursor";
+        cursorTheme.package = pkgs.rose-pine-hyprcursor;
+        theme.name = "adw-gtk3";
+        theme.package= pkgs.adw-gtk3;
+        gtk4.extraCss = ''
+            @import url("file://${builtins.toString config.home.homeDirectory}/.config/gtk-4.0/custom.css");
+        '';
+    };
+    qt = {
+        enable = true;
 
- home.file.".config/rofi/config.rasi".source = dotfiles "rofi-config.rasi";
+        style = {
+            name = "adwaita";           
+            package = pkgs.adwaita-qt;
+        };
 
- home.file.".config/quickshell" = {
- source = dotfiles "quickshell";
- recursive = true;
- };
+        platformTheme.name = "qtct";
+    };
 
- home.file.".config/gtk-3.0/gtk.css".source = dotfiles "gtk.css";
- home.file.".config/gtk-4.0/custom.css".source = dotfiles "gtk.css";
+    xdg.configFile."qt5ct/qt5ct.conf".text = ''
+        [Appearance]
+        style=adwaita
+            icon_theme=BeautyLine
+            '';
 
- gtk = {
- enable = true;
- iconTheme.name = "BeautyLine";
- iconTheme.package = pkgs.beauty-line-icon-theme;
- cursorTheme.name = "rose-pine-hyprcursor";
- cursorTheme.package = pkgs.rose-pine-hyprcursor;
- theme.name = "adw-gtk3";
- theme.package = pkgs.adw-gtk3;
- gtk4.extraCss = ''
- @import url("file://${builtins.toString config.home.homeDirectory}/.config/gtk-4.0/custom.css");
- '';
- };
+    xdg.configFile."qt6ct/qt6ct.conf".text = ''
+        [Appearance]
+        style=adwaita
+            icon_theme=BeautyLine
+            '';
 
- qt = {
- enable = true;
- style = {
- name = "adwaita";
- package = pkgs.adwaita-qt;
- };
- platformTheme.name = "qtct";
- };
-
- xdg.configFile."qt5ct/qt5ct.conf".text = ''
- [Appearance]
- style=adwaita icon_theme=BeautyLine '';
-
- xdg.configFile."qt6ct/qt6ct.conf".text = ''
- [Appearance]
- style=adwaita icon_theme=BeautyLine '';
 }
-
