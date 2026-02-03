e3785367fcd949a4f09a9a9fa6a60bb87a9d067d
Martin Krupicka <martin.x.krupicka@seznam.cz>
2026-02-03 18:58:15 +0100

    Update flake before rebuild

--- home/dotfiles.nix
+++ home/dotfiles.nix
@@ -16,8 +16,25 @@ in
         recursive = true;
     };
 
-    home.file.".config/hypr/" = {
-        source = dotfiles "hypr";
+    home.file.".config/hypr/animations.conf".source = dotfiles "hypr/animations.conf";
+    home.file.".config/hypr/autostart.conf".source = dotfiles "hypr/autostart.conf";
+    home.file.".config/hypr/binds.conf".source = dotfiles "hypr/binds.conf";
+    home.file.".config/hypr/design.conf".source = dotfiles "hypr/design.conf";
+    home.file.".config/hypr/env.conf".source = dotfiles "hypr/env.conf";
+    home.file.".config/hypr/hypridle.conf".source = dotfiles "hypr/hypridle.conf";
+    home.file.".config/hypr/hyprlock.conf".source = dotfiles "hypr/hyprlock.conf";
+    home.file.".config/hypr/hyprpaper.conf".source = dotfiles "hypr/hyprpaper.conf";
+    home.file.".config/hypr/input.conf".source = dotfiles "hypr/input.conf";
+    home.file.".config/hypr/layout.conf".source = dotfiles "hypr/layout.conf";
+    home.file.".config/hypr/monitors.conf".source = dotfiles "hypr/monitors.conf";
+    home.file.".config/hypr/programs.conf".source = dotfiles "hypr/programs.conf";
+    home.file.".config/hypr/rules.conf".source = dotfiles "hypr/rules.conf";
+    home.file.".config/hypr/scripts" = {
+        source = dotfiles "hypr/scripts";
+        recursive = true;
+    };
+    home.file.".config/hypr/wallpapers" = {
+        source = dotfiles "hypr/wallpapers";
         recursive = true;
     };
 # home.file.".config/swaync"= {
