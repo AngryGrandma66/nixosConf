fd798b0264803ab9d3f3cf270df904c4239df0c8
Martin Krupicka <martin.x.krupicka@seznam.cz>
2026-02-04 12:57:35 +0100

    Update flake before rebuild

--- system/steam.nix
+++ system/steam.nix
@@ -1,5 +1,9 @@
-
 { config, pkgs, ... }:
 {
     programs.steam.enable = true;
+    programs.gamescope = {
+        enable = true;
+        capSysNice = true;
+    };
+    programs.steam.gamescopeSession.enable = true;
 }
