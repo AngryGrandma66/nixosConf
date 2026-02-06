5a31995f2d95b6c4f23b8962a6cbe11464b38d1a
Martin Krupicka <martin.x.krupicka@seznam.cz>
2026-02-06 22:05:31 +0100

    Update flake before rebuild

--- system/hyprland.nix
+++ system/hyprland.nix
@@ -1,8 +1,7 @@
 { config, pkgs, ... }:
 {
- services.desktopManager.cosmic.enable = true;
-#  programs.hyprland = {
-#      enable         = true;
-#      xwayland.enable = true;
-#  };
+  programs.hyprland = {
+      enable         = true;
+      xwayland.enable = true;
+  };
  }
